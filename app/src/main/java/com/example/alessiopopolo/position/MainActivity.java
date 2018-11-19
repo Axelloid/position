4b4ecfd61b357e774f16e3e49c9a4fd0794c98f5
Axelloid <alessiopopolo@gmail.com>
2018-11-19 13:33:23 +0100

    Aggiornato a venerdì 16/11

--- app/src/main/java/com/example/alessiopopolo/position/MainActivity.java
+++ app/src/main/java/com/example/alessiopopolo/position/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.alessiopopolo.position;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +15,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
+                Log.d("tag", "Permessi negati");
             // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Log.d("tag", "se sono qui vuol dire che ho già chiesto i permessi, ma me li ha negati");
+                
+                //QUI DEVO SPIEGARE PERCHÈ MI SERVONO, E CHIEDERGLIELI NUOVAMENTE
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+                Log.d("tag", "se sono qui, non ho mai chiesto i permessi");
+                //POSSO SCEGLIERE DI DIRGLI PERCHÈ VOGLIO I PERMESSI PRIMA DI CHIEDERGLIELI
+            }
+        } else {
+            // Permission has already been granted
+            Log.d("tag", "Permessi garantiti");
         }
 
     }
